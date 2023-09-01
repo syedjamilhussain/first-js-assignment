a0538d314e7b7839a2f7b4caf34043118801c69e
[200~https://github.com/syedjamilhussain/js.assignment1.git~ <syedjamilhussain2999@gmail.com>
2023-09-01 23:08:16 +0500

    'changes'

---
+++ index.js
@@ -0,0 +1,51 @@
+alert("Hello How Are YOU" );
+alert("Erroe! please enter a valid pasword");
+alert("Welcome to JS Land... \n Happy Coding!" )
+alert("Welcome to JS Land...");
+  
+function checkCheckbox() {
+    var checkbox = document.getElementById("sampleCheckbox");
+
+    if (checkbox.checked) {
+        alert("Thanks for agreeing!");
+    } else {
+        alert("Please agree to the terms and conditions.");
+    }
+}
+var userResponse = prompt("Happy Coding  \n prevent this page from creating additional dailoges.");
+
+if (userResponse === "yes") {
+    alert("welcome :) !");
+} else {
+    alert("oh its okay !");
+}
+
+var myName = 'Syed Jamil Hussain';
+var userName = ('Hello World');
+
+alert(myName);
+alert(userName);
+
+var userName = ["PIZZzzz \n PIZZzz \n PIZzz \n Piz \n Pz"];
+alert("syedjamilhussain2999@gmail.com");
+
+alert(userName);
+
+var html = document.getElementById('myHTML');
+alert("i am trying to learn from the book A smarter way to learn JavaScript")
+ alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+html.innerHTML = 'I can write in html using javascript';
+function saveAndAlert() {
+    
+    var studentName = document.getElementById("studentName").value;
+    var birthDate = document.getElementById("birthDate").value;
+
+    var certification = document.getElementById("certification") .value;
+    alert("Student's Name: " + studentName);
+    alert("Birth Date: " + birthDate);
+    alert("certification:" + certification)
+}
+ 
+
+  
